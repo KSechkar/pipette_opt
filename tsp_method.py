d54fa56b5ecf899c70d2b54561eeae1bc2ac892f
KSechkar <63181035+KSechkar@users.noreply.github.com>
2020-05-04 16:48:42 +0100

    Add files via upload

--- tsp_method.py
+++ tsp_method.py
@@ -13,7 +13,7 @@ from tspy.solvers.utils import get_cost
 
 #-------------------------------CLASS DEFINITIONS-------------------------------
 #Each reagent stands for a subest of wells which it is added to
-#FOR LEASTOUT: number of outgoing edges is outgoing
+#FOR LEASTOUT: number of outgoing edges is the 'outgoing' variable
 class Ss:
     def __init__(self, reag, wellno): #initialisation
         self.reag=reag
@@ -38,7 +38,13 @@ class Oper:
     def __str__(self): #for printing the subset's reagent type and wells out
         strRep=self.reag+' -> w'+str(self.well)
         return strRep
- 
+    
+#needed for the sametogether reordering
+class Sametogether:
+    def __init__(self,reagtype):
+        self.reagtype=reagtype
+        self.subs=[]
+        self.outgoing=0
 #-------------------------------INPUT-------------------------------
 #Will be replaced by a test exmple generator or manual input reading function
 
@@ -66,7 +72,7 @@ def main():
     #print subsets and D (TEST ONLY)
     disp(subsets, D)
     
-    #reorder the subsets || currently: in random order
+    #reorder the subsets
     leastout(subsets,len(w))
     
     #print subsets and D (TEST ONLY)
@@ -133,8 +139,37 @@ def leastout(subsets,totalwells):
     
     #sort the list
     subsets.sort(key=lambda subsets: subsets.outgoing)
-        
     
+def sametogether(subsets,totalwells):
+    #intialise the 4 lists of same-type reagent subsets
+    together=[Sametogether('p'), Sametogether('r'), Sametogether('c'), Sametogether('t')]
+    
+    #distribute the subsets among the lists, calculate the total number of outgoing edges for each list
+    for i in range(0,len(subsets)):
+        #find into which of 4 list we put it
+        for position in range(0,4):
+            if(subsets[i].reag[0]==together[position].reagtype):
+                break
+        
+        together[position].subs.append(subsets[i]) #record the subset in the proper array
+        together[position].outgoing+=len(subsets[i].wells)*(totalwells-len(subsets[i].wells)) #update number of outgoing edges (for further OPTIONAL sorting)
+        
+    #sort the 4 lists by the number of outgoing edges (OPTIONAL)
+    together.sort(key=lambda together: together.outgoing)
+    
+    #record the rearranged subsets
+    inlist=0 #counter within one of the 4 lists
+    whichlist=0 #which of the 4 lists is current
+    for i in range(0,len(subsets)):
+        subsets[i]=together[whichlist].subs[inlist]
+        inlist+=1
+        if(inlist==len(together[whichlist].subs)):
+            whichlist+=1
+            inlist=0
+            
+    for i in range(0,4):
+        print(together[i].outgoing)
+        
 #-------------------------------SOLVE TSP FOR ONE SUBSET-------------------------------
 #and return the number of tip changes
 def singlesub(subset,D,fin,tipchanges):
